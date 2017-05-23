744dc96a190da746c40ba5e51894a3a9c3706e38
g15946sm <g15946sm@s30408.tsuda.ac.jp>
2017-05-23 13:06:08 +0900

    ドライバー：suisai323 ナビゲーター：tontyan 変更内容：リファクタリングを使ってメソッドを作った。

--- src/kadai5/IntToEng.java
+++ src/kadai5/IntToEng.java
@@ -25,27 +25,8 @@ public class IntToEng {
     	if (n>=0 && n<=10){
     	if (n == 0) {
     		answer = "zero";
-    	} else if (x1 == 1) {
-    		answer = "one";
-    	} else if (x1 == 2) {
-    		answer = "two";
-    	} else if (x1 == 3) {
-    		answer = "three";
-    	} else if (x1 == 4) {
-    		answer = "four";
-    	} else if (x1 == 5) {
-    		answer = "five";
-    	} else if (x1 == 6) {
-    		answer = "six";
-    	} else if (x1 == 7) {
-    		answer = "seven";
-    	} else if (x1 == 8) {
-    		answer = "eight";
-    	} else if (x1 == 9) {
-    		answer = "nine";
-    	} else if (n == 10) {
-    		answer = "ten";
-    	} 
+    	}
+    	answer = digit1(x1, answer);
     	}
     	
     	if (n ==11) {
@@ -68,66 +49,21 @@ public class IntToEng {
     		answer = "nineteen";
     	} 
     	
-    	/*else if (n == 20) {
-    		answer = "twenty";
-    	} else if (n == 30) {
-    		answer = "thirty";
-    	} else if (n == 40) {
-    		answer = "forty";
-    	} else if (n == 50) {
-    		answer = "fifty";
-    	} else if (n == 60) {
-    		answer = "sixty";
-    	} else if (n == 70) {
-    		answer = "seventy";
-    	} else if (n == 80) {
-    		answer = "eighty";
-    	} else if (n == 90) {
-    		answer = "ninety";
-    	}*/
-    	
-    	
     	if (n>=20) {
-    	if (x10 == 2) {
-    		answer2 = "twenty ";
-    	} else if (x10 == 3) {
-    		answer2 = "thirty ";
-    	} else if (x10 == 4) {
-    		answer2 = "forty ";
-    	} else if (x10 == 5) {
-    		answer2 = "fifty ";
-    	}else if (x10 == 6) {
-    		answer2 = "sixty ";
-    	}else if (x10 == 7) {
-    		answer2 = "seventy ";
-    	}else if (x10 == 8) {
-    		answer2 = "eighty ";
-    	}else if (x10 == 9) {
-    		answer2 = "ninety ";
+    	answer2 = digit10(x10, answer2);
+    	
+    	answer = digit1(x1, answer);
     	}
     	
-    	if (x1 == 1) {
-    		answer = "one";
-    	} else if (x1 == 2) {
-    		answer = "two";
-    	} else if (x1 == 3) {
-    		answer = "three";
-    	} else if (x1 == 4) {
-    		answer = "four";
-    	} else if (x1 == 5) {
-    		answer = "five";
-    	} else if (x1 == 6) {
-    		answer = "six";
-    	} else if (x1 == 7) {
-    		answer = "seven";
-    	} else if (x1 == 8) {
-    		answer = "eight";
-    	} else if (x1 == 9) {
-    		answer = "nine";
-    	}
-    	}
+    	answer3 = digit100(x100, answer3);
     	
-    	if (x100 == 1) {
+    	if (n == 1000) answer = "one thousand";
+    	
+        return  answer3 + answer2 + answer;
+    }
+
+	private static String digit100(int x100, String answer3) {
+		if (x100 == 1) {
     		answer3 = "one hundred ";
     	} else if (x100 == 2) {
     		answer3 = "two hundred ";
@@ -146,9 +82,50 @@ public class IntToEng {
     	}else if (x100 == 9) {
     		answer3 = "nine hundred ";
     	}
-    	
-    	if (n == 1000) answer = "one thousand";
-    	
-        return  answer3 + answer2 + answer;
-    }
+		return answer3;
+	}
+
+	private static String digit10(int x10, String answer2) {
+		if (x10 == 2) {
+    		answer2 = "twenty ";
+    	} else if (x10 == 3) {
+    		answer2 = "thirty ";
+    	} else if (x10 == 4) {
+    		answer2 = "forty ";
+    	} else if (x10 == 5) {
+    		answer2 = "fifty ";
+    	}else if (x10 == 6) {
+    		answer2 = "sixty ";
+    	}else if (x10 == 7) {
+    		answer2 = "seventy ";
+    	}else if (x10 == 8) {
+    		answer2 = "eighty ";
+    	}else if (x10 == 9) {
+    		answer2 = "ninety ";
+    	}
+		return answer2;
+	}
+
+	private static String digit1(int x1, String answer) {
+		if (x1 == 1) {
+    		answer = "one";
+    	} else if (x1 == 2) {
+    		answer = "two";
+    	} else if (x1 == 3) {
+    		answer = "three";
+    	} else if (x1 == 4) {
+    		answer = "four";
+    	} else if (x1 == 5) {
+    		answer = "five";
+    	} else if (x1 == 6) {
+    		answer = "six";
+    	} else if (x1 == 7) {
+    		answer = "seven";
+    	} else if (x1 == 8) {
+    		answer = "eight";
+    	} else if (x1 == 9) {
+    		answer = "nine";
+    	}
+		return answer;
+	}
 }
